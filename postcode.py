c2c2d3227aa7a6c7ddad24a64adbaaaba1e380c9
Jaime Visser <jaimevisser@gmail.com>
2023-07-16 20:42:40 +0200

    Calculate all polygons

--- postcode.py
+++ postcode.py
@@ -6,33 +6,39 @@ import yaml
 
 
 class PostalCodeHandler(osmium.SimpleHandler):
-    def __init__(self, postal_code_range):
+    def __init__(self, postal_code_ranges):
         super(PostalCodeHandler, self).__init__()
-        self.points = []
-        self.postal_code_range = postal_code_range
+        self.points = [[] for _ in range(len(postal_code_ranges))]
+        self.postal_code_ranges = postal_code_ranges
 
     def node(self, n):
         if postal_code_tag in n.tags and self._is_valid_postal_code(n.tags[postal_code_tag]):
-            self.points.append((n.location.lon, n.location.lat))
+            postal_code = int(n.tags[postal_code_tag][:4])  # Extract first four digits of postal code
+            for i, (start, end) in enumerate(self.postal_code_ranges):
+                if start <= postal_code <= end:
+                    self.points[i].append((n.location.lon, n.location.lat))
+                    break
 
     def _is_valid_postal_code(self, postal_code):
         code = postal_code[:4]  # Extract first four digits of postal code
-        return code.isdigit() and int(code) in self.postal_code_range
+        return code.isdigit()
 
 
-def extract_outer_boundary(xml_file, postal_code_range):
-    handler = PostalCodeHandler(postal_code_range)
+def extract_outer_boundaries(xml_file, postal_code_ranges):
+    handler = PostalCodeHandler(postal_code_ranges)
     handler.apply_file(xml_file)
+    polygons = []
 
-    if not handler.points:
+    if not handler.points[0]:
         raise ValueError("No points found for the given postal code range.")
 
-    points = np.array(handler.points)
+    for i in handler.points:
+        points = np.array(i)
+        # Calculate the convex hull to get the outer boundary polygon
+        hull = ConvexHull(points)  
+        polygons.append(points[hull.vertices])
 
-    # Calculate the convex hull to get the outer boundary polygon
-    hull = ConvexHull(points)
-
-    return points[hull.vertices]
+    return polygons
 
 
 def draw_polygon(polygon, file_path):
@@ -61,19 +67,18 @@ def draw_polygon(polygon, file_path):
     image.save(file_path, "PNG")
 
 
-
 if __name__ == '__main__':
     with open('config.yaml') as file:
         config = yaml.load(file, Loader=yaml.FullLoader)
 
     xml_file = config['xml_file']
-    postal_code_range = range(config['postal_code_range']['start'], config['postal_code_range']['end'])
+    postal_code_ranges = [(int(r['start']), int(r['end'])) for r in config['postal_code_ranges']]
     postal_code_tag = config['postal_code_tag']
     output_png_file = "output.png"
 
     try:
-        outer_boundary_polygon = extract_outer_boundary(xml_file, postal_code_range)
-        draw_polygon(outer_boundary_polygon, output_png_file)
+        polygons = extract_outer_boundaries(xml_file, postal_code_ranges)
+        draw_polygon(polygons[0], output_png_file)
         print(f"Outer boundary polygon saved as {output_png_file}")
     except ValueError as e:
         print(e)
